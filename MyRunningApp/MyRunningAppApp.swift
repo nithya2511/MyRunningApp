f8a09998959724f1157cfd660258088ec551fd67
Nithya Vasudevan <nithya1vasudevan@gmail.com>
2024-03-14 12:10:59 +0100

    Initial Commit

---
+++ MyRunningApp/MyRunningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRunningAppApp.swift
+//  MyRunningApp
+//
+//  Created by Nithya Vasudevan on 14.03.24.
+//
+
+import SwiftUI
+
+@main
+struct MyRunningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
